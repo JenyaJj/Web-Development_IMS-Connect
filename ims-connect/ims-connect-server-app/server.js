8be05572885ca452fbcf0f51955b04fd8d9b39ad
evgeniiagrekhova <grekhova75@outlook.com>
2024-12-25 22:16:56 +0000

    changed url

--- ims-connect/ims-connect-server-app/server.js
+++ ims-connect/ims-connect-server-app/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5001; 
 
 
 // CORS configuration
